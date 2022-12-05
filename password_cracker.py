d9bb27854def29b47603fd3bc734a17489e4a507
Arun Kumar <53009824+arunkumar-js25@users.noreply.github.com>
2022-12-05 16:07:22 +0000

    5/9 testcases completed

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,19 @@
 import hashlib
 
-def crack_sha1_hash(hash):
-    
+def crack_sha1_hash(hash,use_salts=False):
+  file = open('top-10000-passwords.txt', 'r')
+  Lines = file.readlines()
+  for line in Lines:
+      str = line.replace("\n", "")
+      if(use_salts):
+        saltsfile = open('known-salts.txt', 'r')
+        salts = saltsfile.readlines()
+        for salt in salts:
+          str = salt.replace("\n", "") + line.replace("\n", "") + salt.replace("\n", "")
+          if(hashlib.sha1(str.encode()).hexdigest() == hash):
+            return line.replace("\n", "")
+      else:
+        if(hashlib.sha1(str.encode()).hexdigest() == hash):
+          return str
+
+  return "PASSWORD NOT IN DATABASE"
